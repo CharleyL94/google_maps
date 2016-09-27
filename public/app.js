2328123e34f6095fa3981133fe3e61627d998b4f
Charley Lawton <user@CODECLAN055.local>
2016-09-27 21:38:35 +0100

    buttons not working

--- public/app.js
+++ public/app.js
@@ -1,10 +1,32 @@
+var map;
+
+var handleArthurButtonClick = function(){
+  var coords = {  lat: 55.945101, lng: -3.162346};
+  map.addListener(coords);
+}
+
+var handleMyCurrentLocationClick = function(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    map.addListener({lat: position.coords.latitude, lng:position.coords.longitude})
+  });
+}
+
+
 var initialize = function(){
   var container = document.getElementById('map');
   var center = { lat: 55.945101, lng: -3.162346}
+  var zoom = 15;
+  map = new Map (container, center, zoom);
 
-  var map = new Map(container, center, 10);
+  var arthurButton = document.getElementById("take-me-to-the-top-of-edinburgh");
+  arthurButton.onclick = handleArthurButtonClick;
+
+  var currentLocationButton = document.getElementById("where-am-i");
+  currentLocationButton.onclick = handleMyCurrentLocationClick;
+
   map.addMarker(center);
   map.addClickEvent();
+  
 }
 
 
